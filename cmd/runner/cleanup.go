770cd8873b0d438f5f2f4f5c82cb0f53de088083
Yu Watanabe <yunabe.public@gmail.com>
2017-12-15 21:01:45 +0000

    Delete an unused func: "cmd/runner".MainWithCleanup

--- cmd/runner/cleanup.go
+++ cmd/runner/cleanup.go
@@ -1,16 +1,10 @@
 package runner
 
 import (
-	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
-	"os/exec"
-	"os/signal"
 	"path"
-	"path/filepath"
 	"strings"
-	"syscall"
 )
 
 func cleanSharedLibs(lgopath string, sessID *SessionID) error {
@@ -42,61 +36,4 @@ func CleanSession(gopath, lgopath string, sessID *SessionID) error {
 		return srcErr
 	}
 	return soErr
-}
-
-// MainWithCleanup is a main routine called from wrapper binaries (lgo, lgo-kernel).
-// If killchild is passed, the child process is killed when killchild is closed.
-func MainWithCleanup(lgobin string, killchild <-chan struct{}) {
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		log.Fatal("GOPATH is not set")
-	}
-	lgopath := os.Getenv("LGOPATH")
-	if lgopath == "" {
-		log.Fatal("LGOPATH is empty")
-	}
-	lgopath, err := filepath.Abs(lgopath)
-	if err != nil {
-		log.Fatalf("Failed to get the absolute path of LGOPATH: %v", err)
-	}
-	root := os.Getenv("LGOPATH")
-	if root == "" {
-		log.Fatalf("LGOPATH environ variable is not set")
-		return
-	}
-	// SIGINT: Ctrl-C, SIGTERM: kill, SIGQUIT: Ctrl-\, SIGTSTP: Ctrl-z
-	signal.Notify(make(chan os.Signal), syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGTSTP, syscall.SIGHUP)
-
-	sessID := NewSessionID()
-	binary := path.Join(lgopath, "bin", lgobin)
-	args := []string{fmt.Sprintf("--sess_id=%s", sessID.Marshal())}
-	args = append(args, os.Args[1:]...)
-	cmd := exec.Command(binary, args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-
-	failed := false
-	if err := cmd.Start(); err != nil {
-		log.Printf("Failed to start %s with %v: %v", binary, args, err)
-		failed = true
-	} else {
-		if killchild != nil {
-			// This goroutine leaks! It's okay because this function must be called only once.
-			go func() {
-				<-killchild
-				cmd.Process.Kill()
-			}()
-		}
-		if err := cmd.Wait(); err != nil {
-			log.Printf("%s failed: %v", binary, err)
-			failed = true
-		}
-	}
-	if err := CleanSession(gopath, lgopath, sessID); err != nil {
-		log.Fatalf("Clean-up failure: %v", err)
-	}
-	if failed {
-		os.Exit(1)
-	}
 }
